bdb806b0eb8740d49402da1ae10172462cddd5ab
Justin Schiff <AmbientRevolution@gmail.com>
2015-02-28 14:51:12 -0800

    add jwt decode

--- api/api.js
+++ api/api.js
@@ -24,7 +24,7 @@ app.post('/register', function(req, res) {
 
   var payload = {
     iss: req.hostname,
-    sub: user._id
+    sub: newUser.id
   };
 
   var token = jwt.encode(payload, 'shhhhhh...');
@@ -47,6 +47,16 @@ app.get('/jobs', function(req, res) {
   if (!req.headers.authorization) {
     return res.status(401).send({message: 'You are not authorized'});
   }
+
+  var token = req.headers.authorization.split(' ')[1];
+  var payload = jwt.decode(token, 'shhhhhh...');
+
+  if (!payload.sub) {
+    return res.status(401).send({
+      message: 'You are not authorized'
+    });
+  }
+
   res.json(jobs);
 });
 
