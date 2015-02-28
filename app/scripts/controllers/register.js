59010304a49ad07de0e2bd49687af67fbd304cb4
Justin Schiff <AmbientRevolution@gmail.com>
2015-02-27 18:02:39 -0800

    added an alert service and animate.css

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -1,17 +1,17 @@
 (function(module) {
   'use strict';
 
-  var RegisterCtrl = function ($scope, $http) {
+  var RegisterCtrl = function ($scope, $http, alert) {
     $scope.submit = function() {
       var url = '/';
       var user = {};
 
       $http.post(url, user)
       .success(function(res) {
-        console.log('success');
+        alert('success', 'OK', 'You are now registered.');
       })
       .error(function(err) {
-        console.log('error');
+        alert('warning', 'Oops!', 'Could not register.');
       });
     };
   };
